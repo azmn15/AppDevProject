56724ba5db180c901dd5dd80d790056a5a7c1f8c
fizfiq <33883958+fizfiq@users.noreply.github.com>
2021-11-14 10:50:59 +0800

    make button

---
+++ lib/widget/mybutton.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class MyButton extends StatelessWidget {
+  final Function onPressed;
+  final String name;
+  MyButton({this.name,this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: double.infinity,
+      child: Raisedbutton(
+      child: Text(name),
+      color: Colors.grey,
+      onPressed: onPressed,
+      ),
+    );
+  }
+}
